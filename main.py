cfc0d8c1ddea5f5e93110805532eae27b6c044f8
yeye <Yrios@anexia.mx>
2026-02-09 15:58:44 -0600

    Se modificó version 5 a 7

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 python -m venv aspay
 .\aspay\Scripts\activate
 uvicorn main:app --reload --port 9000
-prueba pablo piña diana Version 5
+prueba pablo piña diana Version 7
 
 """
 from fastapi import FastAPI , Request , HTTPException , Depends
